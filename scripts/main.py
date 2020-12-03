56183a6090bb442b902ff0521e1b20fdfb60dc50
LiOH13 <66305760+LiOH13@users.noreply.github.com>
2020-12-03 23:46:58 +0300

    Update main.py
    add tt

--- scripts/main.py
+++ scripts/main.py
@@ -3,8 +3,10 @@ from datetime import datetime
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+tt = ${{ github.event.inputs.version }}
 
 with open('log.md', 'w') as f:
-    f.write(f'# {timestamp}')
+    #f.write(f'# {timestamp}')
+    f.write(f'# {tt}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
