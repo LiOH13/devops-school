eb39864ba49c778e1642875fdbcc40e4c82037ff
LiOH13 <66305760+LiOH13@users.noreply.github.com>
2020-12-03 18:39:36 +0300

    Update main.py
    Some changes

--- scripts/main.py
+++ scripts/main.py
@@ -6,6 +6,6 @@ timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 with open('log.md', 'w') as f:
     #f.write(f'# {timestamp}')
-    f.write(f'${{ github.event.inputs.version }}')
+    f.write(f'# {github.event.inputs.version}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
