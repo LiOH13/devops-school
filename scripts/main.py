569071379500ccaa2f97909f3205cd4f33308606
LiOH13 <66305760+LiOH13@users.noreply.github.com>
2020-12-04 00:09:10 +0300

    Update main.py
    reset changes

--- scripts/main.py
+++ scripts/main.py
@@ -3,11 +3,8 @@ from datetime import datetime
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-tt = github.event.inputs.version
-print('tt is', tt)
 
 with open('log.md', 'w') as f:
-    #f.write(f'# {timestamp}')
-    f.write(f'# {tt}')
+    f.write(f'# {timestamp}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
