aa4bcfd20b0e5894db2e85d8eb8a6049bf01148c
LiOH13 <66305760+LiOH13@users.noreply.github.com>
2020-12-05 23:48:39 +0300

    Update main.py
    m

--- scripts/main.py
+++ scripts/main.py
@@ -3,10 +3,6 @@ import os
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
-home_dir =os.environ['HOME']
-username = os.environ['USER']
-print(f'{username} home directory is {home_dir}')
-
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 with open('log.md', 'w') as f:
