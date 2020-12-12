3aaac8899177ebc5c6126078f07d2c6ccae823dc
LiOH13 <66305760+LiOH13@users.noreply.github.com>
2020-12-12 23:42:43 +0300

    Update main.py
    test

--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,10 @@ import os
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
+reff = ${{ github.ref }}
+
+print(reff)
+
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 with open('log.md', 'w') as f:
